2116a733e119fc393a10ee527a90a5ccebf6949d
Anderson Sales <andersonsales.mg@gmail.com>
2022-10-12 02:31:56 -0300

    Project files added

---
+++ Modal With Segue/NavigationProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationProject
+//
+//  Created by Anderson Sales on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
+
